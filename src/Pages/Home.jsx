9e6792c70975366a2e30ef732dfff915990d97ba
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-10-08 12:26:12 +0800

    add tailwind to my project

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       <h2>Your one-stop shop for quality wholesale products.</h2>
       <h2>Contact us today to learn more about our offerings!</h2>
       <h2>We look forward to serving you!</h2>
-        <button type="button" className="learn-more-button">Learn More</button>
+        <button type="button" className="learn-more-button" onClick={() => window.location.href='/about'}>Learn More</button>
     </div>
   )
 }
